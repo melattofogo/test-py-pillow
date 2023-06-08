c61ebcabc97fb3bf8e5823fac6604784ba00e7d7
Fogo <melatto.fogo@live.com>
2023-06-08 17:59:26 -0300

    feat(shot.py): implement arguments for file format and file path

--- shot.py
+++ shot.py
@@ -1,14 +1,8 @@
 import pyscreenshot as ImageGrab
 import argparse
+import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="name of the screenshot file",
-                    type=str)
-args = parser.parse_args()
-
-filename = args.filename
-
-def capture_and_crop_screen(crop_positions, output_path):
+def capture_and_crop_screen(crop_positions, output_path, filename, output_format):
     # Capture the screen
     screen = ImageGrab.grab()
 
@@ -18,17 +12,43 @@ def capture_and_crop_screen(crop_positions, output_path):
         cropped_image = screen.crop(position)
         cropped_images.append(cropped_image)
 
-    # Save cropped images as PNG files
-    for i, cropped_image in enumerate(cropped_images):
-        output_file = f"{output_path}/{filename}.png"
-        cropped_image.save(output_file, "PNG")
-        print(f"Saved cropped image {i+1} as {output_file}")
+    # Check for existing files with the same name in the output path
+    existing_files = os.listdir(output_path)
+    existing_files = [f for f in existing_files if f.startswith(f"{filename}-")]
+    index = 0
+    while True:
+        # Generate a unique filename for each cropped image
+        index_str = f"{index:03d}"
+        cropped_filename = f"{filename}-{index_str}.{output_format}"
+        output_file = os.path.join(output_path, cropped_filename)
 
-# Define the positions to crop (left, upper, right, lower)
-crop_positions = [(961, 116, 1918, 1030)]
+        # Save the cropped image if no existing file with the same name
+        if cropped_filename not in existing_files:
+            cropped_images[index % len(cropped_images)].save(output_file, output_format)
+            print(f"Saved cropped image {index + 1} as {output_file}")
+            break
 
-# Define the output path
-output_path = "./output"
+        index += 1
 
-# Call the capture_and_crop_screen function
-capture_and_crop_screen(crop_positions, output_path)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="name of the screenshot file", type=str)
+    parser.add_argument("--output-format", help="output format of the cropped images", type=str, default="png")
+    parser.add_argument("--output-path", help="output path for saving the screenshot files", type=str, default="./output")
+    args = parser.parse_args()
+
+    filename = args.filename
+    output_format = args.output_format
+    output_path = args.output_path
+
+    # Define the positions to crop (left, upper, right, lower)
+    crop_positions = [(961, 116, 1918, 1030)]
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_path, exist_ok=True)
+
+    # Call the capture_and_crop_screen function
+    capture_and_crop_screen(crop_positions, output_path, filename, output_format)
+
+if __name__ == '__main__':
+    main()
